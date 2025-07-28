cb0917ed4c1f52bd823f7cdbabd57dd5871fcd3c
Oleq <oleq.korzhit@gmail.com>
2025-07-28 19:26:50 +0300

    do homework 1 lesson 5

---
+++ lesson_5/hm_1/script.js
@@ -0,0 +1,24 @@
+console.log('for');
+
+for (let i = 20; i <= 30; i += 0.5) {
+	console.log(i);
+}
+
+console.log('while');
+
+let i = 20;
+
+while (i <= 30) {
+	console.log(i);
+	
+	i += 0.5;
+}
+
+console.log('do while');
+
+let ii = 20;
+
+do {
+	console.log(ii);
+	ii += 0.5;
+} while (ii <= 30);
