4df69f98086859fffac4dcd2d1f3da3e4da498c4
Oleq <oleq.korzhit@gmail.com>
2025-08-18 22:38:20 +0300

    add classwork to lesson 9

---
+++ lesson_9/classwork/script.js
@@ -0,0 +1,46 @@
+console.log(orders);
+
+const structuredUsers = (orders) => {
+	return Object.values(orders.reduce((acc, current) => {
+		const userName = current.user;
+		if (acc[userName]) {
+			acc[userName].items = [...acc[userName].items, ...current.items];
+		} else {
+			acc[userName] = structuredClone(current);
+		}
+
+		return acc;
+	}, {}));
+}
+
+const addTotalSumToUser = (users) => {
+	return users.map(obj => {
+		return {
+			...obj,
+			items: [...obj.items],
+			totalSum: obj.items.reduce((acc, current) => acc + current.price, 0),
+		}
+	});
+}
+
+const getUniqueProducts = (users) => {
+	return new Set(users.reduce((acc, current) => {
+		return [...acc, ...current.items];
+	}, []).map(el => el.name));
+}
+
+const getUserWhoSpentMost = (users) => {
+	return users.reduce((acc, current) => {
+		return acc.totalSum < current.totalSum ? current : acc;
+	});
+}
+
+const users = structuredUsers(orders);
+const usersWithTotal = addTotalSumToUser(users);
+const uniqueProducts = getUniqueProducts(usersWithTotal);
+const userSpentMost = getUserWhoSpentMost(usersWithTotal);
+
+console.log(users);
+console.log(usersWithTotal);
+console.log(uniqueProducts);
+console.log(`${userSpentMost.user} потратил больше всех $${userSpentMost.totalSum}`);
