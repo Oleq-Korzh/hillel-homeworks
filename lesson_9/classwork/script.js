230547500a1378269639cede5594d3944ece2f60
Oleq <oleq.korzhit@gmail.com>
2025-08-20 22:38:50 +0300

    add hm

--- lesson_9/classwork/script.js
+++ lesson_9/classwork/script.js
@@ -1,5 +1,3 @@
-console.log(orders);
-
 const structuredUsers = (orders) => {
 	if (orders.length === 0) {
 		return;
@@ -56,7 +54,75 @@ const usersWithTotal = addTotalSumToUser(users);
 const uniqueProducts = getUniqueProducts(usersWithTotal);
 const userSpentMost = getUserWhoSpentMost(usersWithTotal);
 
-console.log(users);
-console.log(usersWithTotal);
-console.log(uniqueProducts);
-console.log(`${userSpentMost.user} потратил больше всех $${userSpentMost.totalSum}`);
+// console.log(users);
+// console.log(usersWithTotal);
+// console.log(uniqueProducts);
+// console.log(`${userSpentMost.user} потратил больше всех $${userSpentMost.totalSum}`);
+
+// MAP
+
+const getValuesOfOrdersEachUsers = (orders) => {
+	if (orders.length === 0) {
+		return;
+	}
+
+	return orders.reduce((acc, current) => {
+
+		if (acc.has(current.user)) {
+			acc.set(current.user, acc.get(current.user) + current.items.length);
+		} else {
+			acc.set(current.user, current.items.length);
+		}
+
+		return acc;
+
+	}, new Map());
+}
+
+const getSumEachUsers = (orders) => {
+	if (orders.length === 0) {
+		return;
+	}
+
+	return orders.reduce((acc, current) => {
+		if (acc.has(current.user)) {
+			acc.set(current.user, acc.get(current.user) + current.items.reduce((a, c) => a += c.price, 0));
+		} else {
+			acc.set(current.user, current.items.reduce((a, c) => a += c.price, 0));
+		}	
+
+		return acc;
+	}, new Map());
+}
+
+const getUniqueProductsWithSet = (orders) => {
+	if (orders.length === 0) {
+		return;
+	}
+
+	// Пришёл к менее избыточному решению чем выше
+
+	return orders.reduce((acc, current) => {
+		current.items.forEach(element => {
+			acc.add(element.name);
+		});
+
+		return acc;
+	}, new Set());
+}
+
+const getUserWhoSpentMostMap = (users) => {
+	if (users.size === 0) {
+		return;
+	}
+
+	return [...users].reduce((acc, current) => {
+		return current[1] > acc[1] ? current : acc;
+	});
+}
+
+const resultOrders = getValuesOfOrdersEachUsers(orders);
+const sumEachUsers = getSumEachUsers(orders);
+const uniqueProductsSet = getUniqueProductsWithSet(orders);
+const userSpentMostMap = getUserWhoSpentMostMap(sumEachUsers);
+console.log(`${userSpentMostMap[0]} потратил больше всех $${userSpentMostMap[1]}`);
