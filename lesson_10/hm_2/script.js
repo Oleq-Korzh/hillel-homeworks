774601b36cc9674fbae2fcf6ddb79e50af24473d
Oleq <oleq.korzhit@gmail.com>
2025-08-21 21:38:08 +0300

    add hm2 to lesson 10

---
+++ lesson_10/hm_2/script.js
@@ -0,0 +1,4 @@
+const numbersArray = [1, 3, 77, 5, 44, 7, 9, 11, 523, 54, 330, 60, 11, 97, 2, 3, 4, 5, 6, 7, 89];
+const pairedNumbersArray = numbersArray.filter(num => num % 2 === 0);
+
+console.log(pairedNumbersArray);
