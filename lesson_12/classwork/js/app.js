26960f141357ae8dd57e987170a7c10d540fa628
Oleq <oleq.korzhit@gmail.com>
2025-09-08 17:28:56 +0300

    do hm to lesson 13

--- lesson_12/classwork/js/app.js
+++ lesson_12/classwork/js/app.js
@@ -80,14 +80,129 @@ const renderProductInfoList = product => {
 	return list;
 }
 
+const renderOrderInfo = (product, objData) => {
+	const orderTable = createHtmlElement({
+		tagName: 'div',
+		attrs: {
+			'class': 'order-table'
+		}
+	});
+
+	orderTable.innerHTML = `
+			<h2>Ваш заказ: </h2>
+			<div>
+				<div class="order-table__row">Название товара: <strong>${product.name}</strong></div>
+				<div class="order-table__row">Полное имя: <strong>${objData.fullName}</strong></div>
+				<div class="order-table__row">Город: <strong>${objData.city}</strong></div>
+				<div class="order-table__row">Доставка: <strong>${objData.payment}</strong></div>
+				<div class="order-table__row">Комментарий: <strong>${objData.comment}</strong></div>
+			</div>
+			<h3>Сумма к оплате ${product.price}$</h3>
+	`;
+
+	return orderTable;
+};
+
+const resetForm = (form, formParent, className) => {
+	const formError = formParent.querySelectorAll('.error');
+	form.reset();
+	formParent.classList.add(className);
+
+	formError.forEach(el => el.remove());
+}
+
+const createError = (text) => {
+	if (!text) {
+		return;
+	}
+
+	return createHtmlElement({
+		tagName: 'span',
+		text: text,
+		attrs: {
+			'class': 'error'
+		}
+	});
+}
+
+const validateFormValue = (string) => {
+	const regex = /^.+$/;
+
+	return regex.test(string.trim());
+}
+
 const productTableInit = () => {
 	const categoriesCol = document.querySelector('.categories');
 	const productsCol = document.querySelector('.products');
 	const infoCol = document.querySelector('.info');
+	const orderFormWrapperEl = document.querySelector('.order-form');
+	const orderForm = document.forms.order;
+	const orderFormSubmit = orderForm.querySelector('button');
 	const orderEl = document.querySelector('.order-details');
+	const FORM_HIDDEN_CLASS = 'hidden';
 
-	if (!categoriesCol || !productsCol || !infoCol || !orderEl) {
+	if (!categoriesCol || !productsCol || !infoCol || !orderEl || !orderFormWrapperEl || !orderForm) {
 		return;
+	}
+
+	orderFormWrapperEl.classList.add(FORM_HIDDEN_CLASS);
+
+	const showForm = (product) => {
+		orderFormWrapperEl.classList.remove(FORM_HIDDEN_CLASS);
+
+		orderFormSubmit.addEventListener('click', () => {
+			const formData = {
+				fullName: orderForm.fullname.value,
+				city: [...orderForm.city].find(el => el.selected).value,
+				delivery: orderForm.delivery.value,
+				payment: [...orderForm.payment].find(el => el.checked).value,
+				comment: orderForm.comment.value,
+			}
+
+			const isNotValidate = !Object.values(formData).every(formVal => validateFormValue(formVal));
+
+			if (isNotValidate) {
+				const errorEl = orderFormWrapperEl.querySelector('.error');
+
+				if (!errorEl) {
+					const error = createError('Ошибка, проверьте форму на правильность!');
+
+					orderFormWrapperEl.appendChild(error);
+				}
+
+				return;
+			}
+
+			const orderInfoEl = renderOrderInfo(product, formData);
+
+			orderEl.appendChild(orderInfoEl);
+		});
+	}
+
+	const showProductsByCategory = category => {
+		[productsCol, infoCol, orderEl].forEach(el => el.replaceChildren());
+		resetForm(orderForm, orderFormWrapperEl, FORM_HIDDEN_CLASS);
+		const productsList = renderProductsList(category);
+
+		productsCol.appendChild(productsList);
+
+		productsList.addEventListener('click', event => {
+			if (event.target && event.target.closest('li')) {
+				const productId = parseInt(event.target.dataset.product);
+
+				if (!productId) {
+					return;
+				}
+
+				const product = category.products.find(product => product.id === productId);
+
+				if (!product) {
+					return;
+				}
+
+				showProductInfo(product);
+			}
+		});
 	}
 
 	const showCategories = () => {
@@ -114,41 +229,16 @@ const productTableInit = () => {
 		});
 	}
 
-	const showProductsByCategory = category => {
-		[productsCol, infoCol, orderEl].forEach(el => el.replaceChildren());
-
-		const productsList = renderProductsList(category);
-
-		productsCol.appendChild(productsList);
-
-		productsList.addEventListener('click', event => {
-			if (event.target && event.target.closest('li')) {
-				const productId = parseInt(event.target.dataset.product);
-
-				if (!productId) {
-					return;
-				}
-
-				const product = category.products.find(product => product.id === productId);
-
-				if (!product) {
-					return;
-				}
-
-				showProductInfo(product);
-			}
-		});
-	}
-
 	const showProductInfo = product => {
 		[infoCol, orderEl].forEach(el => el.replaceChildren());
+		resetForm(orderForm, orderFormWrapperEl, FORM_HIDDEN_CLASS);
 		const infoList = renderProductInfoList(product);
 
 		infoCol.appendChild(infoList);
 
 		infoList.addEventListener('click', event => {
 			if (event.target && event.target.closest('button')) {
-				orderEl.textContent = `Поздравляю! Вы успешно купили ${product.name}`;
+				showForm(product);
 			}
 		});
 	}
