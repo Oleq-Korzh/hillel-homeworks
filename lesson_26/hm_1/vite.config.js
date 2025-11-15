f8a0456c94f447c9c1148e7abb38a71206df3d19
Oleg Korzh <oleq.korzhit@gmail.com>
2025-11-15 12:55:54 +0200

    fix

--- lesson_26/hm_1/vite.config.js
+++ lesson_26/hm_1/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hillel-homeworks/lesson_26/hm_1/",
+  base: "/hillel-homeworks/",
 });
